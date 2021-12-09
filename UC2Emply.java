231f445933d883dff3cd121ab9d5003a118cf152
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-09 18:16:00 +0530

    UC2Emply

---
+++ UC2Emply.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UC2Emply {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter number of hours worked: ");
+            Scanner sc = new Scanner(System.in);
+                double totalHoursWorked = sc.nextInt();
+                double standardWage = 20;
+                int standardHours = 40;
+                double totalWage;
+
+            if (totalHoursWorked < 40)
+                totalWage = standardWage * totalHoursWorked;
+            else
+                totalWage = standardWage * standardHours;
+
+            System.out.println("Your total salary of the week: " + totalWage);
+    }
+}
