53e037875cb9b03c5406f9805973a95a5e2fd716
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-09 18:41:01 +0530

    UC2Listcomp

---
+++ UC2ListComp.java
@@ -0,0 +1,13 @@
+class UC2ListComp{
+public static void main(String args[])
+{
+    String str1 = "hi My name is ashwath ";
+    String str2 = "";
+    String str3 = "last name is naidu";
+
+        System.out.println(str1.compareTo(str2));
+
+        System.out.println(str2.compareTo(str3));
+
+    }
+}
